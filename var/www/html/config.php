8b585a71bd0a948e3f4e5c2b0f8d865f7b80dd85
Willis Yonker <wyonker@gmail.com>
2022-06-24 11:46:21 -0400

    Delete config.php

--- var/www/html/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $DBHost = "localhost";
-    $DBUser = "primal";
-    $DBPass = "primal";
-    $DBName = "primal";
-    $conn = mysql_connect($DBHost, $DBUser, $DBPass) or die
-                         ('Error connecting to mysql');
-    mysql_select_db($DBName);
-?>
