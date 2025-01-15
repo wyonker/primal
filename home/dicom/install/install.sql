ba6410f85941f8f5c4288409c3a9abd5865df22f
wyonker <wyonker@gmail.com>
2025-01-15 15:18:27 -0500

    DB updates.  Lots...

--- home/dicom/install/install.sql
+++ home/dicom/install/install.sql
@@ -26,17 +26,19 @@ create table process (puid varchar(24) not null,
                       tendproc datetime,
                       perror int(1) DEFAULT 0,
 					  primary key (puid, pservername));
-create table send    (puid varchar(24) not null,
+create table send    (id bigint(10) UNSIGNED UNIQUE not null AUTO_INCREMENT,
+					  puid varchar(24) not null,
 					  sservername varchar(64) not null,
 					  tdestnum tinyint,
-					  index (puid),
                       tdest varchar(38) not null,
+					  org varchar(5) not null default "UNK",
                       tstartsend datetime not null,
                       tendsend datetime,
                       timages int(6),
                       serror int(1) not null default 0,
 					  sretry int(1) not null default 0,
-					  complete int(1));
+					  complete int(1),
+					  index (puid));
 create table user	 (loginid varchar(36) not null primary key,
 					  login_sec_level int(5) not null,
 					  username varchar(36) not null,
@@ -65,7 +67,7 @@ create table series  (SIUID varchar(68) not null,
                       SERIUID varchar(68),
                       puid varchar(24) not null,
                       SeriesDesc varchar(256),
-					  SeriesNumImg int(8),
+					  SeriesNumImg int not null default 1,
 					  Modality varchar(4),
 					  primary key (puid, SERIUID));
 create table image   (SOPIUID varchar(68) not null,
@@ -118,7 +120,7 @@ create table Prefetch (prefetch_id int(10) UNSIGNED UNIQUE not null AUTO_INCREME
 create table conf_rec (conf_rec_id bigint(20) UNSIGNED UNIQUE not null AUTO_INCREMENT,
 				      conf_name varchar(64) not null default 'default',
 					  conf_server varchar(128) not null default 'localhost',
-					  rec_type varchar(32) not null default 'dicom',
+					  rec_type varchar(32) not null default '1',
 					  rec_port int(5) default 2000,
 					  rec_dir varchar(128) not null default '/home/dicom/receive',
 					  rec_log_full_path varchar(128) not null default '/var/log/primal/receive.log',
@@ -128,7 +130,7 @@ create table conf_rec (conf_rec_id bigint(20) UNSIGNED UNIQUE not null AUTO_INCR
 					  proc_dir varchar(128) not null default '/home/dicom/process',
 					  proc_log_full_path varchar(128) not null default '/var/log/primal/process.log',
 					  out_dir varchar(128) not null default '/home/dicom/send',
-					  out_comp_level int(5) not null default 0,
+					  rec_comp_level int(5) not null default 6,
 					  out_log_full_path varchar(128) not null default '/var/log/primal/send.log',
 					  sent_dir varchar(128) not null default '/home/dicom/sent',
 					  hold_dir varchar(128) not null default '/home/dicom/hold',
@@ -146,13 +148,14 @@ create table conf_proc (conf_proc_id int(10) UNSIGNED UNIQUE not null AUTO_INCRE
 					  proc_operator varchar(32) not null default '=',
 					  proc_cond varchar(32) not null default '0',
 					  proc_action varchar(8) not null default 'allow',
-					  proc_order int(5) not null default 0, 
+					  proc_order int(5) not null default 1, 
 					  proc_dest int(10) default 0,
 				      active int(1) not null default 1,
 				      primary key (conf_proc_id));
 create table conf_send (conf_send_id int(10) UNSIGNED UNIQUE not null AUTO_INCREMENT,
 				      conf_rec_id bigint(20) UNSIGNED not null default 0,
 				      send_name varchar(64) not null default 'default',
+					  send_org varchar(5) not null default 'UNK',
 					  send_aet varchar(32) not null default 'PRIMAL',
   					  send_aec varchar(32) not null default 'PRIMAL',
 					  send_hip varchar(32) not null default '0.0.0.0',
@@ -163,8 +166,13 @@ create table conf_send (conf_send_id int(10) UNSIGNED UNIQUE not null AUTO_INCRE
 					  send_retry int(5) not null default 3,
 					  send_username varchar(32) not null default 'primal',
 					  send_password varchar(32) not null default 'primal',
+					  send_order int(5) not null default 1,
 				      active int(1) not null default 1,
 				      primary key (conf_send_id));
+create table config (id int UNSIGNED UNIQUE not null AUTO_INCREMENT,
+					  conf_name varchar(64) not null default 'default',
+					  conf_value varchar(64) not null default '0',
+  	 				  PRIMARY KEY (id));
 create table page_columns (id int(4) not null primary key,
 					  name varchar(32) not null,
 					  dorder int(4) not null);
@@ -236,4 +244,21 @@ insert into page_columns (id, name, dorder) values(18, 'Case ID', 18);
 insert into locker (pproc, plock) values ('config_edit', '0');
 create index image_puid_indx on image(puid);
 create index series_modality on series(Modality);
+insert into config (conf_name, conf_value) values ('use_db', '0');
+INSERT INTO conf_rec SET conf_name = '!Global!', conf_server = 'localhost', rec_type = '1', rec_port = 2000, rec_dir = '/home/dicom/receive', rec_log_full_path = '/var/log/primal/receive.log', rec_log_level = 'debug', rec_aet = 'PRIMAL', rec_time_out = 10, proc_dir = '/home/dicom/process', proc_log_full_path = '/var/log/primal/process.log', out_dir = '/home/dicom/send', rec_comp_level = 6, out_log_full_path = '/var/log/primal/send.log', sent_dir = '/home/dicom/sent', hold_dir = '/home/dicom/hold', error_dir = '/home/dicom/error', dupe = 0, pass_through = 0, ret_period = 4320, active = 1;
+INSERT INTO conf_proc SET conf_rec_id = (SELECT conf_rec_id FROM conf_rec WHERE conf_name = '!Global!' LIMIT 1), proc_name = '!Global!', proc_type = 'tag', proc_tag = '0010,0010', proc_operator = '=', proc_cond = '%', proc_action = 'allow', proc_order = 0, proc_dest = 0, active = 1;
+INSERT INTO conf_send SET conf_rec_id = (SELECT conf_rec_id FROM conf_rec WHERE conf_name = '!Global!' LIMIT 1), send_name = '!Global!', send_aet = 'PRIMAL', send_aec = 'PRIMAL', send_hip = '0.0.0.0', send_type = 'dicom', send_port = 104, send_time_out = 10, send_comp_level = 0, send_retry = 3, send_username = 'primal', send_password = 'primal', send_order = 0, active = 1;
 
+DROP DATABASE IF EXISTS mirth_primal;
+CREATE DATABASE IF NOT EXISTS mirth_primal;
+use mirth_primal;
+create table rec (id bigint(10) UNSIGNED UNIQUE not null AUTO_INCREMENT,
+                rec_date datetime not null default "1970-01-01 00:00:00",
+				accn varchar(38) not null default 0,
+				org varchar(5) not null default "UNK",
+				study_date datetime not null default "1970-01-01 00:00:00",
+                siuid varchar(60) not null default 0,
+                send_date datetime not null default "1970-01-01 00:00:00",
+				send_status int not null default 0,
+				INDEX accn (accn));
+grant all privileges on mirth_primal.* to 'primal'@'localhost' identified by 'ThisIsMirth1!' with grant option;
