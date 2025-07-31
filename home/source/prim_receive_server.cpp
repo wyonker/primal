ea18a2a418a0e5f6e45cfdae71335b3b752e823a
wyonker <wyonker@gmail.com>
2025-07-31 13:29:48 -0400

    Fixed 2

--- home/source/prim_receive_server.cpp
+++ home/source/prim_receive_server.cpp
@@ -253,6 +253,8 @@ std::size_t fStartReceive(std::string strMessage) {
     if(fs::exists("/etc/primal/prim_ae_map.conf")) {
         strLogMessage = strPrimalID + " RECV " + "Searching conf file for client ID of " + strAEC + "...";
         fWriteLog(strLogMessage, conf1.primConf[strRecNum + "_PRILOGDIR"] + "/" + conf1.primConf[strRecNum + "_PRILFIN"]);
+        strLogMessage = strPrimalID + " RECV " + "message is " + strMessage;
+        fWriteLog(strLogMessage, conf1.primConf[strRecNum + "_PRILOGDIR"] + "/" + conf1.primConf[strRecNum + "_PRILFIN"]);
         std::cout << strLogMessage << std::endl;
         std::ifstream fpAEMap("/etc/primal/prim_ae_map.conf");
         while (std::getline(fpAEMap, strLine)) {
