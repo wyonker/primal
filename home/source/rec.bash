7845255b987b1b12cc0fe4d126b5f01738e1ed9d
wyonker <wyonker@gmail.com>
2025-09-03 17:11:25 -0400

    rec.bash 3

--- home/source/rec.bash
+++ home/source/rec.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Should get path, rec_id, aet, aec
-#REV 3
+#REV 4
 
 FULLPATH=`echo "$1"|cut -d " " -f1`
 RECID=`echo "$1"|cut -d " " -f2`
 SENDERAET=`echo "$1"|cut -d " " -f3`
 CALLINGAET=`echo "$1"|cut -d " " -f4`
 
-PUID=`echo "$1"|rev|cut -d '/' -f1|rev`
+PUID=`echo "$FULLPATH"|rev|cut -d '/' -f1|rev`
 
 echo "INSERT INTO receive SET puid = $PUID, fullpath = $FULLPATH, rservername = `hostname`, rec_id = $RECID, tstartrec = NOW(), senderAET = $SENDERAET, callingAET = $CALLINGAET;" | mysql -N -u root primal
