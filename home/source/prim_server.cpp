f04b95076bf795000a3781a282dcae056b0a764e
wyonker <wyonker@gmail.com>
2025-09-08 11:48:09 -0400

    fix startup 3

--- home/source/prim_server.cpp
+++ home/source/prim_server.cpp
@@ -64,7 +64,7 @@ std::vector<std::string > vecRCact1;
 MYSQL *mconnect;
 MYSQL *mconnect2;
 
-const std::string strVersionNum = "4.01.31";
+const std::string strVersionNum = "4.01.32";
 const std::string strVersionDate = "2025-09-08";
 
 //const std::string strProcChainType = "PRIMRCSEND";
@@ -409,8 +409,7 @@ int fStartReceivers() {
                 //Need to start the receive process.
                 if(strType == "1") {
                     strCMD = "/home/dicom/bin/storescp --fork +cl " + strRecCompLevel + " -aet " + strAET + " -tos " + strTO + " -ll " + strLL + " -od " + strDir;
-                    //strCMD += " -ss " + strRecID + " -xf /home/dicom/bin/storescp.cfg Default -fe \".dcm\" -xcr \"/home/dicom/rec.bash \\\"#p " + strRecID + " #a #c #f\\\" >> /var/log/primal/rec.log 2>&1\" " + strPort + " >> " + strLog + " 2>&1 &";
-                    strCMD += " -ss " + strRecID + " -xf /home/dicom/bin/storescp.cfg Default -fe \".dcm\" -xcr \"/home/dicom/rec.bash #p " + strRecID + " #a #c #f\" " + strPort + " >> /var/log/primal/rec.log 2>&1 >> " + strLog + " 2>&1 &";
+                    strCMD += " -ss " + strRecID + " -xf /home/dicom/bin/storescp.cfg Default -fe \".dcm\" -xcr \"/home/dicom/rec.bash #p " + strRecID + " #a #c #f >> /var/log/primal/rec.log 2>&1\" " + strPort + " >> " + strLog + " 2>&1 &";
                     strLogMessage = "SRECV  " + strCMD;
                     fWriteLog(strLogMessage, "/var/log/primal/primal.log");
                     strStatus = exec(strCMD.c_str());
