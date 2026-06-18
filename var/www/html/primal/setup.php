558e378f961f1dc58a02d6aabe7100b03cd97962
Willis Yonker <wyonker@gmail.com>
2026-06-18 14:10:37 -0400

    Update setup.php

--- var/www/html/primal/setup.php
+++ var/www/html/primal/setup.php
@@ -1021,6 +1021,19 @@ if($_GET['action'] == 'Rec') {
 		echo '<td><input type="text" name="send_username" value ="' . $row["send_username"] . '" /></td></tr>';
 		echo '<tr><td>' . 'Password:</td>';
 		echo '<td><input type="text" name="send_password" value ="' . $row["send_password"] . '" /></td></tr>';
+		echo '<tr><td>' . 'Encrypt on store?:</td><td>';
+		echo '<select name="encrypt" id="encrypt" default=2>';
+		echo '<option value="1"';
+		if($row["encrypt"] == 1) {
+			echo ' selected';
+		}
+		echo '>Yes</option>';
+		echo '<option value="2"';
+		if($row["encrypt"] == 2) {
+			echo ' selected';
+		}		
+		echo '>No</option>';
+		echo '</select></td></tr>';
 		echo '<tr><td>' . '<label for="active">Active?:</label></td><td>';
 		echo '<select name="active" id="active" default=1>';
 		echo '<option value="1"';
