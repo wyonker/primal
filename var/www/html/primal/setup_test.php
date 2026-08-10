0e421291fa0a23eaa6113cd2f1f5e6b790a04503
Willis Yonker <wyonker@gmail.com>
2026-08-10 14:36:19 -0400

    Update setup_test.php

--- var/www/html/primal/setup_test.php
+++ var/www/html/primal/setup_test.php
@@ -1,7 +1,7 @@
 <?php
 	//License GPLv3
-	//Version 1.00.00
-	//2026-08-05
+	//Version 1.00.01
+	//2026-08-10
     session_start();
     header( "Expires: Mon, 20 Dec 1998 01:00:00 GMT" );
     header( "Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT" );
@@ -41,15 +41,61 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
+Display_Header2();
+echo "<H2>System Setup</H2>";
+echo '<H3><a href="setup_user.php">Setup User</a></H3>';
+echo '<H3><a href="setup_db.php">Setup Database</a></H3>';
+echo '<H3><a href="setup_restart.php">Restart PRIMAL</a></H3>';
+if($_GET['action'] != 'defaults') {
+	echo '<H3><a href="setup.php?action=defaults">Modify Default Settings</a></H3>';
+} else {
+	echo '<H3><a href="setup.php">Normal</a></H3>';
+}
 if($_GET['action'] == 'Rule') {
     $intRule = $_GET['rec'];
-    $sql = "SELECT * FROM conf_proc WHERE conf_proc_id = '$intRule'";
-    $result = mysqli_query($conn, $sql);
+    $strQuery = "SELECT * FROM conf_proc WHERE conf_proc_id = '$intRule'";
+    $result = mysqli_query($conn, $strQuery);
     $row = mysqli_fetch_assoc($result);
     $strTestConditionRule = $row["proc_cond"];
     $strTestActionCondition = $row["proc_action_value"];
 
-    echo '<form method="post" action="setup_test.php?action=Rule&rec=' . $intRule . '">';
+    echo '<form method="post" action="setup_test.php?action=Rule2&rec=' . $intRule . '">';
+    echo '<table>';
+    echo '<tr><td>Condition Value to be tested:</td>';
+    echo '<td><input type="text" name="condition_value" value ="' . $strTestConditionValue . '" /></td></tr>';
+    echo '<tr><td>' . 'Rule Condition:</td>';
+    echo '<td><input type="text" name="condition_rule" value ="' . $strTestConditionRule . '" /></td></tr>';
+    echo '<tr><td>Result:</td>';
+    echo '<td><input type="text" name="condition_result" value ="' . $strTestConditionResult . '" /></td></tr>';
+    echo '<br>';
+    echo '<tr><td>Action Value to be tested:</td>';
+    echo '<td><input type="text" name="action_value" value ="' . $strTestActionValue . '" /></td></tr>';
+    echo '<tr><td>' . 'Rule Condition:</td>';
+    echo '<td><input type="text" name="action_condition" value ="' . $strTestActionCondition . '" /></td></tr>';
+    echo '<tr><td>Result:</td>';
+    echo '<td><input type="text" name="action_result" value ="' . $strTestActionResult . '" /></td></tr>';
+    echo '<br>';
+    echo '<tr><td><input type="submit" name="btnTest" value="Test" /></td></tr>';
+    echo '</table>';
+    echo '</form>';
+}elseif($_GET['action'] == 'Rule2') {
+    $intRule = $_GET['rec'];
+    $strQuery = "SELECT * FROM conf_proc WHERE conf_proc_id = '$intRule'";
+    $result = mysqli_query($conn, $strQuery);
+    $row = mysqli_fetch_assoc($result);
+    $strTestConditionRule = $row["proc_cond"];
+    $strTestActionCondition = $row["proc_action_value"];
+
+    $strQuery = "SELECT conf_value FROM config WHERE conf_name = 'test_rule_condition_result'";
+    $result = mysqli_query($conn, $strQuery);
+    $row = mysqli_fetch_assoc($result);
+    $strTestConditionResult = $row["conf_value"];
+    $strQuery = "SELECT conf_value FROM config WHERE conf_name = 'test_rule_action_result'";
+    $result = mysqli_query($conn, $strQuery);
+    $row = mysqli_fetch_assoc($result);
+    $strTestActionResult = $row["conf_value"];
+    
+    echo '<form method="post" action="setup_test.php?action=Rule2&rec=' . $intRule . '">';
     echo '<table>';
     echo '<tr><td>Condition Value to be tested:</td>';
     echo '<td><input type="text" name="condition_value" value ="' . $strTestConditionValue . '" /></td></tr>';
